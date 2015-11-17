43feb93b0c276dd3dd4971c8c72cca1ecbf8ed3a
charlie <charlie@localhost>
2015-11-17 09:16:48 +0100

    Formateado Controlador

--- src/AppBundle/Controller/LuckyController.php
+++ src/AppBundle/Controller/LuckyController.php
@@ -4,36 +4,34 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
 //use Symfony\Component\HttpFoundation\Request;
 
 class LuckyController extends Controller {
+
     /**
      * @Route("/lucky/number/{count}")
      */
     public function numberAction($count) {
-        
+
         $numbers = array();
         for ($i = 0; $i < $count; $i++) {
             $numbers[] = rand(0, 100);
         }
-        
+
         $numbersList = implode(', ', $numbers);
-//
+
 //        $html = $this->container->get('templating')->render(
 //            'lucky/number.html.twig',
 //            array('luckyNumberList' => $numbersList)
 //        );
-//
 //        return new Response($html);
-        
-        
         // render: a shortcut that does the same as above
         return $this->render(
-        'lucky/number.html.twig',
+                        'lucky/number.html.twig',
 //        'lucky/number.html.php',
-        array('luckyNumberList' => $numbersList)
-    );
-        
+                        array('luckyNumberList' => $numbersList)
+        );
     }
 
     // ...
